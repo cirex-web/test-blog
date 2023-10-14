fd076315a7973e48ba9a17e8e543319ddd9e9f3a
topshenyi-web <erxu@ctemc.org>
2023-10-14 12:38:59 -0400

    improve css for main page and blog post page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
 import { getAllPosts, getPostData } from "../util";
 import css from "./index.module.css";
+import backArrowIcon from "@/../public/arrow-back.svg";
+import Link from "next/link";
 export default async function Page({ params }: { params: { id: string } }) {
   const { title, html, date } = await getPostData(params.id);
   // console.log(html);
   return (
     <div className={css.container}>
-      <h2>{title}</h2>
+      <Link href={"/"}>
+        <div className={css.navigation}>
+          <Image src={backArrowIcon} alt="back arrow" height={20} />
+          <p>Back</p>
+        </div>
+      </Link>
+      <h2 className={css.title}>{title}</h2>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
