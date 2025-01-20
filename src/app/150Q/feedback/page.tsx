b9819b69386f726b809436c8f48744e6e0321af8
topshenyi-web <erxu@ctemc.org>
2025-01-19 22:14:37 -0500

    feat: add 150 redirects

---
+++ src/app/150Q/feedback/page.tsx
@@ -0,0 +1,4 @@
+import { redirect } from "next/navigation";
+export default async function Home({ params }: any) {
+  redirect("https://forms.gle/EFMNTVKCuyUqzNfS8");
+}
---
+++ src/app/150Q/submit/page.tsx
@@ -0,0 +1,4 @@
+import { redirect } from "next/navigation";
+export default async function Home({ params }: any) {
+  redirect("https://forms.gle/n3Wz8om6tQvTMZEZ8");
+}
