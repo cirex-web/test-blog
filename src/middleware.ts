6a0007fe8fd6829ea866bcbc49c3f2178769fac2
topshenyi-web <erxu@ctemc.org>
2025-11-11 09:30:53 -0500

    chore: redirect to journal

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,11 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  return NextResponse.redirect(
+    new URL(
+      "https://fluttering-rooster-dbc.notion.site/journal-2a857280f0eb80109614de3f3f73e302"
+    )
+  );
   console.log(request.nextUrl.pathname);
   if (request.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/home/history", request.url));
